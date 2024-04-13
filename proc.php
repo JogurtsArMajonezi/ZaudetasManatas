602c3767aa7ceaff9cf3d9cad40b55936850304f
JogurtsArMajonezi <toms.graudums@marupe.edu.lv>
2024-04-13 15:16:25 +0300

    A lot changes
    mainiju login un vel kko biku

---
+++ proc.php
@@ -0,0 +1,15 @@
+<?php
+$l = $_POST["login"];
+$p = $_POST["password"];
+
+include("cfg.php");
+$result = $mysqli->query("SELECT * FROM users WHERE login = '$l' and password ='$p'");
+if (mysqli_num_rows($result) > 0){
+    echo "GOOOOD!!!";
+}
+else{
+    echo "BYE BYE!!!";
+}
+
+
+?>
