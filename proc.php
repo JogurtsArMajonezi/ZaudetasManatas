926df5ba5d0a9f2f35a6b251d888aa01c673f6b6
JogurtsArMajonezi <toms.graudums@marupe.edu.lv>
2024-04-16 18:42:12 +0300

    Pievienot+ register sakums
    asdasdasdasdasdasdasd

--- proc.php
+++ proc.php
@@ -5,10 +5,10 @@ $p = $_POST["password"];
 include("cfg.php");
 $result = $mysqli->query("SELECT * FROM users WHERE login = '$l' and password ='$p'");
 if (mysqli_num_rows($result) > 0){
-    echo "GOOOOD!!!";
+    echo "All good!";
 }
 else{
-    echo "BYE BYE!!!";
+    echo "Try again!";
 }
 
 
